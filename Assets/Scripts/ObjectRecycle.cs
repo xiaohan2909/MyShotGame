bcd06319cffa9edfb03bc4c6430f461dd6dea9e7
lixiaohan <415832014@qq.com>
2016-04-14 15:05:06 +0800

    提交版本

---
+++ Assets/Scripts/ObjectRecycle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 用于把摄像机左边的景物搬运到摄像机右边，并且随机一个位置
+/// </summary>
+public class ObjectRecycle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		//如果景物已经在摄像机的左边了而且摄像机也看不到它了
+		if(! this.transform.renderer.IsVisibleFrom(Camera.main) && this.transform.position.x<Camera.main.transform.position.x){
+			float dx=Random.Range(-3,17);
+			float dy=Random.Range(-3,4);
+			Vector3 adaptVector=new Vector3(Camera.main.transform.position.x+20,Camera.main.transform.position.y,this.transform.position.z);
+			this.transform.position=adaptVector+new Vector3(dx,dy,0);
+		}
+	}
+}
---
+++ Assets/Scripts/RepeatDisapper.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+/// <summary>
+///自动创建敌人用的一个类
+/// </summary>
+public class RepeatDisapper : MonoBehaviour {
+	public bool IsCreateEnemy=true;
+	public Transform newEnemy;
+	public static RepeatDisapper Instance;
+	void Awake(){
+		// Register the singleton
+		if (Instance != null)
+		{
+			Debug.LogError("Multiple instances of RepeatDisapper!");
+		}
+		Instance = this;
+	}
+	/// <summary>
+	///自动创建敌人,位置随机
+	/// positon 位置 num 数量
+	/// </summary>
+	public void createEnemy(Vector3 positon, int num){
+		for(int i=0;i<num;i++){
+			float dx=Random.Range(-3,17);
+			float dy=Random.Range(-3,4);
+			positon.z=this.transform.position.z; positon.x+=dx; positon.y+=dy;
+			var enemyTransform = Instantiate(newEnemy) as Transform;
+			MoveScript move = enemyTransform.gameObject.GetComponent<MoveScript>();
+			enemyTransform.position=positon;
+			move.speed=new Vector2(5,5);
+			move.direction=new Vector2(-1,0);
+		}//for
+	}
+}
